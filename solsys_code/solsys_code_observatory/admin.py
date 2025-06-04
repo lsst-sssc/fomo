730ed1dbfd405ed28a04f57f521527b0cd3a6bdf
Tim Lister <tlister@lco.global>
2025-06-04 10:20:55 -0700

    Add Observatory into the admin

--- solsys_code/solsys_code_observatory/admin.py
+++ solsys_code/solsys_code_observatory/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from solsys_code.solsys_code_observatory.models import Observatory
+
+
+class ObservatoryAdmin(admin.ModelAdmin):  # noqa: D101
+    model = Observatory
+    list_display = ['obscode', 'name', 'lon', 'lat', 'altitude']
+    ordering = ['obscode']
+
+
+admin.site.register(Observatory, ObservatoryAdmin)
