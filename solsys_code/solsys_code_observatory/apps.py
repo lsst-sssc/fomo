a14438553884da110bd3880ef60c3e6eed516c73
Tim Lister <tlister@lco.global>
2025-06-17 22:06:02 -0700

    WIP: Add stub (from tom_demoapp) for adding Observatory's to nav bar

--- solsys_code/solsys_code_observatory/apps.py
+++ solsys_code/solsys_code_observatory/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class SolsysCodeObservatoryConfig(AppConfig):  # noqa: D101
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'solsys_code.solsys_code_observatory'
+
+    def nav_items(self):
+        """
+        Integration point for adding items to the navbar.
+        This method should return a list of dictionaries that include a `partial` key pointing to the html templates to
+        be included in the navbar. The `position` key, if included, should be either "left" or "right" to specify which
+        side of the navbar the partial should be included on. If not included, a left side nav item is assumed.
+        """
+        # TODO: These filenames probably don't need 'demo' in them b/c they're namespaced in the app folder
+        # XXX: Map the '.' in the module hierarchy to a '/'
+        return [
+            {'partial': f'{self.name}/partials/navbar_demo.html', 'position': 'right'},
+            {'partial': f'{self.name}/partials/navbar_list_demo.html'},
+        ]
