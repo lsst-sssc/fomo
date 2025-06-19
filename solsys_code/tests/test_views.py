fa7ade915f4744a05b85e4be31727242476e5c7c
Tim Lister <tlister@lco.global>
2025-06-19 14:59:06 -0700

    First cut at a query service for the JPL Small-Body Identification API
    Basic class to build a query around a FOV center,time along with (some)
    unit tests. Should probably be split into a sub-app and/or refactored
    into a `tom_dataservices` module At Some Point.

---
+++ solsys_code/tests/test_views.py
@@ -0,0 +1,84 @@
+# Disable logging during testing
+import logging
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+from django.test import SimpleTestCase
+
+# Import code to test
+from solsys_code.views import JPLSBId
+
+logger = logging.getLogger(__name__)
+# Disable anything below CRITICAL level
+logging.disable(logging.CRITICAL)
+
+
+class TestJPLSBId(SimpleTestCase):
+    def setUp(self) -> None:
+        self.test_rubin = JPLSBId(mpc_code='X05', fov_ra_hwidth=1.75 * u.deg, fov_dec_hwidth=1.75 * u.deg)
+        self.root_url = 'https://ssd-api.jpl.nasa.gov/sb_ident.api'
+        self.base_url = self.root_url + '?mpc-code=X05&mag-required=true&two-pass=false'
+
+        self.maxDiff = None
+        return super().setUp()
+
+    def test_basic_init(self):
+        foo = JPLSBId()
+
+        self.assertEqual(self.test_rubin.mpc_code, foo.mpc_code)
+        self.assertEqual(self.test_rubin.fov_ra_hwidth, foo.fov_ra_hwidth)
+        self.assertEqual(self.test_rubin.fov_dec_hwidth, foo.fov_dec_hwidth)
+
+    def test_build_base_query_defaults(self):
+        expected_url = self.root_url + '?mpc-code=X05&mag-required=true&two-pass=false'
+
+        url = self.test_rubin._build_base_query()
+
+        self.assertEqual(expected_url, url)
+
+    def test_build_base_query_defaults_mag_false(self):
+        expected_url = self.root_url + '?mpc-code=X05&mag-required=false&two-pass=false'
+
+        self.test_rubin.mag_required = False
+
+        url = self.test_rubin._build_base_query()
+
+        self.assertEqual(expected_url, url)
+
+    def test_build_base_query_defaults_2pass_false(self):
+        expected_url = self.root_url + '?mpc-code=X05&mag-required=true&two-pass=true'
+
+        self.test_rubin.two_pass = True
+
+        url = self.test_rubin._build_base_query()
+
+        self.assertEqual(expected_url, url)
+
+    def test_query_center_positive_dec(self):
+        expected_url = (
+            self.base_url
+            + '&obs-time=2020-01-01T11:10:01&fov-ra-center=10-10-10&fov-dec-center=42-05-10'
+            + '&fov-ra-hwidth=1.75&fov-dec-hwidth=1.75'
+        )
+
+        obs_time = Time('2020-01-01T11:10:01', scale='utc')
+        center = SkyCoord('10h10m10s +42d05m10s', frame='icrs')
+
+        url = self.test_rubin.query_center(obs_time, center)
+
+        self.assertEqual(expected_url, url)
+
+    def test_query_center_negative_dec(self):
+        expected_url = (
+            self.base_url
+            + '&obs-time=2020-01-01T11:10:01&fov-ra-center=10-10-10&fov-dec-center=M42-05-10'
+            + '&fov-ra-hwidth=1.75&fov-dec-hwidth=1.75'
+        )
+
+        obs_time = Time('2020-01-01T11:10:01', scale='utc')
+        center = SkyCoord('10h10m10s -42d05m10s', frame='icrs')
+
+        url = self.test_rubin.query_center(obs_time, center)
+
+        self.assertEqual(expected_url, url)
--- solsys_code/views.py
+++ solsys_code/views.py
@@ -1,3 +1,70 @@
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+
 # from django.shortcuts import render
 
-# Create your views here.
+
+class JPLSBId:
+    """
+    The ``JPLSBId`` provides an interface to JPL's Small Body Identification Tool
+    via its API interface (https://ssd-api.jpl.nasa.gov/doc/sb_ident.html)
+    """
+
+    # XXX better in settings or a conf?
+    base_url = 'https://ssd-api.jpl.nasa.gov/sb_ident.api'
+
+    @u.quantity_input
+    def __init__(
+        self,
+        mpc_code='X05',
+        fov_ra_hwidth: u.Quantity[u.deg] = 1.75 * u.deg,
+        fov_dec_hwidth: u.Quantity[u.deg] = 1.75 * u.deg,
+    ) -> None:
+        self.mpc_code = mpc_code.upper()
+        if self.mpc_code == '500':
+            raise ValueError("MPC site code '500' (geocenter) is not valid for this service")
+        self.fov_ra_hwidth = fov_ra_hwidth.to(u.deg)
+        self.fov_dec_hwidth = fov_dec_hwidth.to(u.deg)
+        self.two_pass = False  # whether to request the 2nd numerical integration pass rather than two-body model
+        self.mag_required = True  # skip objects without magnitude parameters
+
+    def _build_base_query(self):
+        """
+        Build the base query from things we already know about such as the site code etc
+        """
+
+        url = f'{self.base_url}?mpc-code={self.mpc_code}&mag-required={str(self.mag_required).lower()}'
+        url += f'&two-pass={str(self.two_pass).lower()}'
+        return url
+
+    @u.quantity_input
+    def query_center(self, obs_time: Time, center: SkyCoord):
+        """
+        Query for small bodies around <center> (a SkyCoord in the ICRS frame) at <obs_time> (a Time instance)
+        """
+
+        if not isinstance(obs_time, Time):
+            obs_time = Time(obs_time, scale='utc')
+        if not isinstance(center, SkyCoord):
+            if isinstance(center, [tuple, list]):
+                center = SkyCoord(center[0], center[1], frame='icrs')
+            else:
+                raise ValueError('center must be a SkyCoord or a pair of values that can initialize one')
+        print(f'Querying around ({center.ra.deg:.3f}, {center.dec.deg:+.2f}) at {obs_time.utc} UTC')
+        url = self._build_base_query()
+        # XXX may need '_' not 'T', docs inconsistent
+        time_fmt = '%Y-%m-%dT%H:%M:%S'
+        url += f'&obs-time={obs_time.utc.to_datetime().strftime(time_fmt)}'
+        # Add RA string
+        url += f"&fov-ra-center={center.ra.to_string(u.hourangle, sep='-', precision=0, pad=True)}"
+        # Add Dec string
+        dec_string = center.dec.to_string(u.deg, sep='-', precision=0, pad=True)
+        # Turn minus sign into 'M' needed by API
+        dec_string = dec_string[0].replace('-', 'M') + dec_string[1:]
+        url += f'&fov-dec-center={dec_string}'
+        # Add RA and Dec half-widths
+        url += f'&fov-ra-hwidth={self.fov_ra_hwidth.to(u.deg).value:.2f}'
+        url += f'&fov-dec-hwidth={self.fov_dec_hwidth.to(u.deg).value:.2f}'
+
+        return url
