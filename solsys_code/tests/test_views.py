e9b43c0c1c1819b4d838e74a64c761f4bdcde18d
Tim Lister <tlister@lco.global>
2025-07-24 17:15:09 -0700

    Correct converted state vector from heliocentric->barycentric.
    Results now agrees with JPL Horizons at the milliarcsec level.
    Added some tests for `convert_target_to_layup` (could do with more)

---
+++ solsys_code/tests/test_views.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+
+import numpy as np
+from django.test import TestCase, tag
+from numpy.testing import assert_almost_equal
+from tom_targets.models import Target
+
+# Import module to test
+from solsys_code.views import convert_target_to_layup
+
+MJD_TO_JD_CONVERSION = 2400000.5
+
+
+class TestConvertTargetToLayup(TestCase):
+    def setUp(self) -> None:
+        epochJD_TDB = 2457545.5
+        target_params = {
+            'name': 'Fake Rock',  # actually (3666) Holman
+            'type': Target.NON_SIDEREAL,
+            'scheme': 'MPC_COMET',
+            'eccentricity': 0.1273098035049758,
+            'perihdist': 2.719440725596252,
+            'inclination': 2.363582123773087,
+            'lng_asc_node': 120.3869311659506,
+            'arg_of_perihelion': 55.06308037812056,
+            'epoch_of_perihelion': 57934.05265870551,
+            'epoch_of_elements': epochJD_TDB - MJD_TO_JD_CONVERSION,
+        }
+        self.target, created = Target.objects.get_or_create(**target_params)
+
+        # barycentric cartesian (equatorial) - these are the reference values for comparison
+        x_bary_eq = -7.195156074800051e-02
+        y_bary_eq = 2.800941663957977e00
+        z_bary_eq = 1.148299189842545e00
+        vx_bary_eq = -9.914826873812788e-03
+        vy_bary_eq = -1.508913222991139e-03
+        vz_bary_eq = -2.356455160257992e-04
+        self.bary_vec = np.array([x_bary_eq, y_bary_eq, z_bary_eq, vx_bary_eq, vy_bary_eq, vz_bary_eq])
+        # Sun's position at epochJD_TDB (from Horizons Vector Table with
+        # Target Body: 10 (Sun), Coord Center: @0 (SSB))
+        Sun = namedtuple('Sun', 'x y z vx vy vz')
+        sun_epoch = Sun(
+            x=3.743893517879733e-03,
+            y=2.355922092887896e-03,
+            z=8.440770737482685e-04,
+            vx=-7.096646739414067e-07,
+            vy=6.421467712437571e-06,
+            vz=2.788964122162865e-06,
+        )
+        self.sun_dict = {epochJD_TDB: sun_epoch}
+
+    def test_provided_sun_dict(self):
+        converted = convert_target_to_layup(self.target, self.sun_dict)
+        for name, j in zip(converted.dtype.names[2:8], range(6)):
+            assert_almost_equal(converted[name], self.bary_vec[j], 8)
+
+    @tag('spiceypy')
+    def test_sun_ephemeris(self):
+        """test conversion with in-place Sun position determination
+        This tests needs SPICE kernels available (TBD but at least:
+        * naif0012.tls (leap second kernel)
+        * de440s.bsp (Some version of DE440 ephemeris)
+        )"""
+
+        converted = convert_target_to_layup(self.target)
+        for name, j in zip(converted.dtype.names[2:8], range(6)):
+            assert_almost_equal(converted[name], self.bary_vec[j], 8)
--- solsys_code/views.py
+++ solsys_code/views.py
@@ -1,7 +1,9 @@
+from collections import namedtuple
 from pathlib import Path
 
 import numpy as np
 import pooch
+import spiceypy as spice
 from astropy import units as u
 from astropy.constants import GM_sun
 from astropy.time import Time
@@ -10,6 +12,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
 from layup.convert import get_output_column_names_and_types
 from layup.predict import predict
+from layup.utilities.data_processing_utilities import layup_furnish_spiceypy
 from sorcha.ephemeris.orbit_conversion_utilities import universal_cartesian
 from sorcha.ephemeris.simulation_geometry import ecliptic_to_equatorial
 from tom_targets.models import Target
@@ -17,22 +20,36 @@ from tom_targets.models import Target
 from solsys_code.solsys_code_observatory.models import Observatory
 
 cache_dir = Path(pooch.os_cache('layup'))
+# "Furnish" (load) SPICE kernels
+layup_furnish_spiceypy(cache_dir)
 
 
-def convert_target_to_layup(target):
+def convert_target_to_layup(target, sun_dict=None):
     """Converts a `Target` to a numpy array in format needed for 'layup'
 
     Args:
         target (tom_targets.model.Target): Target
+        sun_dict (dict): [Optional] A dict with a key of a JD_TDB pointing
+            at a dict of {x,y,z,vx,vy,vz} for position and velocity of the
+            Sun to override the internal rebound determination
 
     Returns:
         output (numpy structured array): Data converted to layup input format
     """
 
+    # Numerical constants needed
     # Solar Gravitional constant (converted to au/days)
     mu_sun = GM_sun.to(u.au**3 / u.day**2).value
+    # mu_sun = 2.9591220828559115e-04 # value from tests
     # mu_total = 0.00029630927487993194
     mu = mu_sun
+    # Value of au in meters (fixed by IAU 2012 resolution)
+    AU_M = 149597870700
+    AU_KM = AU_M / 1000.0
+    SEC_PER_DAY = 24 * 60 * 60
+
+    if sun_dict is None:
+        sun_dict = {}
     # Transform Target orbital elements to cartesian state vector
     time_peri_tdb = Time(target.epoch_of_perihelion, format='mjd', scale='tdb')
     epoch_tdb = Time(target.epoch_of_elements, format='mjd', scale='tdb')
@@ -70,16 +87,41 @@ def convert_target_to_layup(target):
     # First we convert our data into equatorial barycentric cartesian coordinates,
     # regardless of the input format. That allows us to simplify the conversion
     # process below by only having the logic to convert from BCART_EQ to the other formats.
-    # sun = ephem.get_particle("Sun", d["epochMJD_TDB"] + MJD_TO_JD_CONVERSTION - ephem.jd_ref)
-    # Need to add Sun's position vector as elements above from MPC/JPL are heliocentric
-    # not barycentric. Later...
 
-    # Convert to Barycentric Cartesian (equatorial) by converting from ecliptic coordinates
+    # Convert to Heliocentric Cartesian (equatorial) by converting from ecliptic coordinates
     ecliptic_coords = np.array((x, y, z))
     ecliptic_velocities = np.array((vx, vy, vz))
 
     equatorial_coords = np.array(ecliptic_to_equatorial(ecliptic_coords))
     equatorial_velocities = np.array(ecliptic_to_equatorial(ecliptic_velocities))
+    # Convert from heliocentric->barycentric using the Sun's position and velocity
+    # This can either be passed in as [sun_dict] (e.g. for testing) or we can
+    # determine it ourselves using plain SPICE calls
+    if epoch_tdb.jd not in sun_dict:
+        # Rebound:
+        # from layup.utilities.layup_configs import LayupConfigs
+        # from sorcha.ephemeris.simulation_setup import _create_assist_ephemeris
+        # configs = LayupConfigs()
+        # ephem, gm_sun, gm_total = _create_assist_ephemeris(configs.auxiliary, cache_dir)
+        # sun_dict[epoch_tdb.jd] = ephem.get_particle("Sun", epoch_tdb.jd - ephem.jd_ref)
+        et_sun = spice.str2et(f'jd {epoch_tdb.jd} tdb')
+        sun_posvel, sun_ltt = spice.spkezr('SUN', et_sun, 'J2000', 'NONE', 'SSB')
+        # Convert from km and km/s to AU and AU/day
+        sun_posvel /= AU_KM
+        sun_posvel[3:6] *= SEC_PER_DAY
+        Sun = namedtuple('Sun', 'x y z vx vy vz')
+        sun_dict[epoch_tdb.jd] = Sun(
+            x=sun_posvel[0],
+            y=sun_posvel[1],
+            z=sun_posvel[2],
+            vx=sun_posvel[3],
+            vy=sun_posvel[4],
+            vz=sun_posvel[5],
+        )
+
+    sun = sun_dict[epoch_tdb.jd]
+    equatorial_coords += np.array((sun.x, sun.y, sun.z))
+    equatorial_velocities += np.array((sun.vx, sun.vy, sun.vz))
     row = tuple(np.concatenate([equatorial_coords, equatorial_velocities]))
     row += (epoch_tdb.mjd,)
     row += tuple(0 for col, _ in cols_to_keep)
