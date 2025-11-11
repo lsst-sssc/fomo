aef958fb22a76b8042924ebb16f3bca5e5a6fe35
Tim Lister <tlister@lco.global>
2025-11-11 15:19:42 -0800

    Add ades_data (from read_psv) to Table converter

--- solsys_code/tests/test_utils.py
+++ solsys_code/tests/test_utils.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
+from astropy.table import Table
 from django.test import SimpleTestCase
 
-from solsys_code.utils import read_psv
+from solsys_code.utils import convert_ades_to_table, read_psv
 
 
 class TestReadPSV(SimpleTestCase):
@@ -74,3 +75,44 @@ class TestReadPSV(SimpleTestCase):
         self.assertEqual(len(expected_data[0]), len(psv_data['keywords']))
         for row in [psv_data['observations'][0], psv_data['observations'][-1]]:
             self.assertEqual(row, expected_data.pop(0))
+
+
+class TestConvertADESToTable(SimpleTestCase):
+    def setUp(self) -> None:
+        self.test_psv_file = Path(__file__).parent / 'test_data' / 'sample.psv'
+        self.ades_data = read_psv(self.test_psv_file)
+        return super().setUp()
+
+    def test_convert_ades_to_table(self):
+        ades_table = convert_ades_to_table(self.ades_data)
+        self.assertEqual(Table, type(ades_table))
+        self.assertEqual(len(ades_table), 36)
+        expected_columns = [
+            'permID',
+            'provID',
+            'trkSub',
+            'mode',
+            'stn',
+            'obsTime',
+            'ra',
+            'dec',
+            'rmsRA',
+            'rmsDec',
+            'astCat',
+            'mag',
+            'rmsMag',
+            'band',
+            'fltr',
+            'photCat',
+            'photAp',
+            'logSNR',
+            'seeing',
+            'exp',
+            'rmsFit',
+            'nStars',
+            'notes',
+            'remarks',
+        ]
+        self.assertEqual(len(ades_table.colnames), len(expected_columns))
+        for col in ades_table.colnames:
+            self.assertIn(col, expected_columns)
