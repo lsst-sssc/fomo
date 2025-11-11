6f8670d76e737b9caa913ebe42cc98bd8ae7b0a2
Tim Lister <tlister@lco.global>
2025-11-11 14:29:36 -0800

    First attempts at an ADES PSV file reader and parser

---
+++ solsys_code/tests/test_utils.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+
+from django.test import SimpleTestCase
+
+from solsys_code.utils import read_psv
+
+
+class TestReadPSV(SimpleTestCase):
+    def setUp(self) -> None:
+        self.test_psv_file = Path(__file__).parent / 'test_data' / 'sample.psv'
+        return super().setUp()
+
+    def test_read_psv(self):
+        expected_items = ['header', 'keywords', 'observations']
+        expected_data = [
+            [
+                '217P',
+                '',
+                '',
+                'CCD',
+                'Z24',
+                '2025-11-11T05:27:02.7Z',
+                '138.036656',
+                '+12.403909',
+                '0.144',
+                '0.100',
+                'Gaia3',
+                '18.78',
+                '0.270',
+                'G',
+                'g',
+                'Gaia3',
+                '1.4',
+                '1.45',
+                '1.1',
+                '170',
+                '0.17',
+                '332',
+                '',
+                '',
+            ],
+            [
+                '217P',
+                '',
+                '',
+                'CCD',
+                'Z24',
+                '2025-11-11T05:43:44.6Z',
+                '138.038423',
+                '+12.403937',
+                '0.119',
+                '0.072',
+                'Gaia3',
+                '17.59',
+                '0.244',
+                'G',
+                'i',
+                'Gaia3',
+                '2.9',
+                '1.94',
+                '1.1',
+                '170',
+                '0.13',
+                '474',
+                '',
+                '',
+            ],
+        ]
+
+        psv_data = read_psv(self.test_psv_file)
+        self.assertEqual(len(expected_items), len(list(psv_data.keys())))
+        for key in psv_data:
+            self.assertIn(key, expected_items)
+        self.assertEqual(len(expected_data[0]), len(psv_data['keywords']))
+        for row in [psv_data['observations'][0], psv_data['observations'][-1]]:
+            self.assertEqual(row, expected_data.pop(0))
