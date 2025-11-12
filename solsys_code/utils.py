e85d799ffe37671f3396094e19f654d9cc37beec
Tim Lister <tlister@lco.global>
2025-11-12 11:40:00 -0800

    Add zero_aperture_extrapolation routine to perform the extrapolation (and make plots if desired)

--- solsys_code/utils.py
+++ solsys_code/utils.py
@@ -1,6 +1,7 @@
 import logging
 import re
 
+import numpy as np
 from ades import adesutility
 from ades.psvtoxml import (
     DATA_RECORD_LINE,
@@ -11,10 +12,14 @@ from ades.psvtoxml import (
     stateTransitions,
 )
 from astropy.table import Table
+from matplotlib import pyplot as plt
 
 logger = logging.getLogger(__name__)
 
 allowedElementDict, requiredElementDict, psvFormatDict = adesutility.getAdesTables()
+
+# A lot of this ADES PSV parsing code is adapted from `psvtoxml.py` in the `ades` package
+# but somewhat simplified for our purposes here and to avoid carrying lots of `globals` about
 
 #
 # create regular expression matching a string with no
@@ -109,11 +114,20 @@ def parsePSVLine(line):
 
 def parsePSV(psvline, stack):
     """Parses a single line of a PSV format ADES file
+    Takes, updates and returns the "stack" dictionary being built up from the
+    PSV file (which in our case is a simpler dictionary as we're not building an XML)
+    Modified from the original in `psvtoxml.py` in the `ades` package to:
+    1) simplify the "stack" to just a dictionary which is passed in and updated
+    2) remove the use of global variables except for state tracking
+    3) add version checking for the first line
+    4) doesn't have the same state transition complexity and checks as the original
 
     :param psvline: line from PSV file to be parsed
     :type psvline: str
-    :return: _description_
-    :rtype: _type_
+    :param stack: The "stack" dictionary being built up from the PSV file.
+    :type stack: dict
+    :return: The "stack" dictionary being built up from the PSV file.
+    :rtype: dict
     """
 
     global state
@@ -168,12 +182,12 @@ def parsePSV(psvline, stack):
 def read_psv(psvfile, psvencoding='UTF-8'):
     """Reads a PSV format ADES file
 
-    :param psvfile: _description_
-    :type psvfile: _type_
+    :param psvfile: Filepath to PSV file
+    :type psvfile: str
     :param psvencoding: PSV file encoding, defaults to "UTF-8"
     :type psvencoding: str, optional
-    :return: _description_
-    :rtype: _type_
+    :return: Dictionary with 'header', 'keywords' and 'observations' keys
+    :rtype: dict
     """
 
     global state
@@ -235,3 +249,122 @@ def convert_ades_to_table(ades_data):
     table = Table(rows=ades_data['observations'], names=ades_data['keywords'], dtype=dtypes)
 
     return table
+
+
+def zero_aperture_extrapolation(ades_table, reference_ap=1.8, make_plots=False):
+    """Perform the linear extrapolation to zero aperture size for RA and Dec
+       for the passed ADES table `ades_table`. The values at the reference aperture
+       size `reference_ap` (in arcsec) are used as the base values.
+    XXX this needs to be checked for in the apertures avalilable in the table
+
+    :param table: Table of ADES observations from convert_ades_to_table()
+    :type table: astropy.table.Table
+    :param reference_ap: Reference aperture size in arcseconds, defaults to 1.8
+    :type reference_ap: float, optional
+    :param make_plots: Whether to generate plots of the linear fits, defaults to False
+    :type make_plots: bool, optional
+    :return: Dictionary of derived zero-aperture RA and Dec values for each obsTime
+    :rtype: dict
+    """
+
+    from astropy import table
+
+    marker_col = 'k'
+    zaa_col = '#1f77b4'
+
+    derived_zaa = {}
+    unique_dts = table.unique(ades_table, keys='obsTime')
+    for dt in unique_dts['obsTime']:
+        mask = ades_table['obsTime'] == dt
+        sub_table = ades_table[mask]
+        # Determine target name; assume it's the same for all entries at this obsTime
+        # work along the permID, provID, trkSub columns to find the first non-empty value
+        target_name = 'Unknown'
+        for col in ['permID', 'provID', 'trkSub']:
+            if col in sub_table.colnames:
+                non_empty = sub_table[col][sub_table[col] != '']
+                if len(non_empty) > 0:
+                    target_name = non_empty[0]
+                    break
+
+        if 'ra' in sub_table.colnames and 'dec' in sub_table.colnames:
+            apertures = sub_table['photAp']
+            ras = sub_table['ra'] - sub_table['ra'][apertures == reference_ap][0]
+            cosdec = np.cos(np.deg2rad(sub_table['dec'][apertures == reference_ap][0]))
+            ras = ras * cosdec * 3600  # Convert to arcseconds
+            ra_errs = sub_table['rmsRA'] * cosdec
+            decs = sub_table['dec'] - sub_table['dec'][apertures == reference_ap][0]
+            decs = decs * 3600  # Convert to arcseconds
+            dec_errs = sub_table['rmsDec']
+            if len(apertures) >= 2:
+                # Perform linear fit to ra vs. photAp
+                coeffs_ra = np.polyfit(apertures, ras, 1)
+                # Extrapolate to zero aperture size
+                delta_zero_ap_ra = np.polyval(coeffs_ra, 0)
+                zero_ap_ra = sub_table['ra'][apertures == reference_ap][0] + (delta_zero_ap_ra / 3600.0) / cosdec
+                print(f'Extrapolated RA for obsTime {dt}: {zero_ap_ra:.7f}')
+                # Perform linear fit to dec vs. photAp
+                coeffs_dec = np.polyfit(apertures, decs, 1)
+                # Extrapolate to zero aperture size
+                delta_zero_ap_dec = np.polyval(coeffs_dec, 0)
+                zero_ap_dec = sub_table['dec'][apertures == reference_ap][0] + (delta_zero_ap_dec / 3600.0)
+                print(f'Extrapolated Dec for obsTime {dt}: {zero_ap_dec:+.7f}')
+                # Make summary plots of the extrapolated values
+                if make_plots is True:
+                    fig, (ax1, ax2) = plt.subplots(2, 1, dpi=150)
+                    # Reserve space on the right of the figure for the external legend
+                    fig.subplots_adjust(right=0.78)
+                    fig.suptitle(f'{target_name} -- {dt} - Linear Fit')
+                    xmax = np.ceil(apertures.max())
+                    x = [0.0, apertures.min(), xmax]
+                    coeffs_arr = np.polyval(coeffs_ra, x)
+
+                    ax1.plot(x[1:], coeffs_arr[1:], color=marker_col, label='RA Fit')
+                    ax1.scatter(0.0, delta_zero_ap_ra, marker='o', color=zaa_col, label='0 Aperture\nExtrapolation')
+                    ax1.plot(x[0:2], coeffs_arr[0:2], color=zaa_col, linestyle='--')
+                    ax1.errorbar(
+                        apertures,
+                        ras,
+                        yerr=ra_errs,
+                        marker='d',
+                        color=marker_col,
+                        linestyle='',
+                        label='Included RA Data',
+                    )
+                    # Remove the offset from both x and y axes, turn on minor ticks for y axis
+                    ax1.ticklabel_format(useOffset=False, axis='both')
+                    ax1.yaxis.minorticks_on()
+                    ax1.set_ylabel(r'$\Delta \text{RA}*\cos(\text{Dec})$ (arcsec)')
+                    ax1.set_title(f'RA: {zero_ap_ra:.7f}' + r'$^\circ$')
+                    ax1.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0)
+
+                    # Same again for Dec
+                    coeffs_arr = np.polyval(coeffs_dec, x)
+                    ax2.plot(x[1:], coeffs_arr[1:], color=marker_col, label='Dec Fit')
+                    ax2.scatter(0.0, delta_zero_ap_dec, marker='o', color=zaa_col, label='0 Aperture\nExtrapolation')
+                    ax2.plot(x[0:2], coeffs_arr[0:2], color=zaa_col, linestyle='--')
+                    ax2.errorbar(
+                        apertures,
+                        decs,
+                        yerr=dec_errs,
+                        marker='s',
+                        markersize=8,
+                        color=marker_col,
+                        linestyle='',
+                        label='Included Dec Data',
+                    )
+                    ax2.set_title(f'Dec: {zero_ap_dec:+.7f}' + r'$^\circ$')
+                    # Remove the offset from both x and y axes, turn on minor ticks for y axis
+                    ax2.ticklabel_format(useOffset=False, axis='both')
+                    ax2.yaxis.minorticks_on()
+                    ax2.set_xlabel('Aperture Size (arcsec)')
+                    ax2.set_ylabel(r'$\Delta \text{Dec}$ (arcsec)')
+                    ax2.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0)
+                    fig.tight_layout()
+                derived_zaa[str(dt)] = {
+                    'zero_ap_ra': zero_ap_ra,
+                    'zero_ap_dec': zero_ap_dec,
+                    'delta_zero_ap_ra': delta_zero_ap_ra,
+                    'delta_zero_ap_dec': delta_zero_ap_dec,
+                }
+    return derived_zaa
