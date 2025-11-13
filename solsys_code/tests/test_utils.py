ced6df6f638acce7c070394ab196208526bca13c
Tim Lister <tlister@lco.global>
2025-11-12 16:56:28 -0800

    Add (basic) test for zero_aperture_extrapolation

--- solsys_code/tests/test_utils.py
+++ solsys_code/tests/test_utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from astropy.table import Table
 from django.test import SimpleTestCase
 
-from solsys_code.utils import convert_ades_to_table, read_psv
+from solsys_code.utils import convert_ades_to_table, read_psv, zero_aperture_extrapolation
 
 
 class TestReadPSV(SimpleTestCase):
@@ -116,3 +116,37 @@ class TestConvertADESToTable(SimpleTestCase):
         self.assertEqual(len(ades_table.colnames), len(expected_columns))
         for col in ades_table.colnames:
             self.assertIn(col, expected_columns)
+
+
+class TestZeroApertureExtrapolation(SimpleTestCase):
+    def setUp(self) -> None:
+        test_psv_file = Path(__file__).parent / 'test_data' / 'sample.psv'
+        self.ades_data = read_psv(test_psv_file)
+        self.ades_table = convert_ades_to_table(self.ades_data)
+        return super().setUp()
+
+    def test_zero_aperture_extrapolation(self):
+        expected_dts = [
+            '2025-11-11T05:27:02.7Z',
+            '2025-11-11T05:30:22.0Z',
+            '2025-11-11T05:33:42.3Z',
+            '2025-11-11T05:37:02.8Z',
+            '2025-11-11T05:40:24.0Z',
+            '2025-11-11T05:43:44.6Z',
+        ]
+
+        derived_zaa = zero_aperture_extrapolation(self.ades_table, make_plots=False)
+        self.assertEqual(len(derived_zaa), len(self.ades_table) // 6)  # 6 frames, 6 apertures per frame
+        self.assertEqual(expected_dts, list(derived_zaa.keys()))
+        # Check known values (pre-computed)
+        expected_zaa_ra_dec = [
+            (138.036628, 12.403883),
+            (138.037016, 12.403832),
+            (138.037354, 12.403898),
+            (138.037738, 12.404076),
+            (138.038080, 12.403972),
+            (138.038430, 12.403934),
+        ]
+        for expected_ra_dec, values in zip(expected_zaa_ra_dec, derived_zaa.values()):
+            self.assertAlmostEqual(expected_ra_dec[0], values['zero_ap_ra'], places=6)
+            self.assertAlmostEqual(expected_ra_dec[1], values['zero_ap_dec'], places=6)
