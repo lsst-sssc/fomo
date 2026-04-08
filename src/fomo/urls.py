de69920ecd78d7ac92e8c310f2b531b446a9ff8c
Tim Lister <tlister@lco.global>
2026-04-08 16:42:47 -0700

    Add include of tom_jpl.urls experimental HTMX sortable table view

--- src/fomo/urls.py
+++ src/fomo/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('observatory/', include('solsys_code.solsys_code_observatory.urls', namespace='solsys_code_observatory')),
     path('ephem/<int:pk>/', Ephemeris.as_view(), name='ephem'),
     path('targets/<int:pk>/makeephem/', MakeEphemerisView.as_view(), name='makeephem'),
+    path('tom_jpl/', include('tom_jpl.urls', namespace='tom_jpl')),
     path('', include('tom_common.urls')),
 ]
